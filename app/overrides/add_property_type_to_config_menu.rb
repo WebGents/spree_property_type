e17462b494ad90d678d74aec9d5867858cadf36d
dragsbaek <tdragsbaek@hotmail.com>
2017-04-08 04:39:54 +0200

    get route from spree

--- app/overrides/add_property_type_to_config_menu.rb
+++ app/overrides/add_property_type_to_config_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
     :virtual_path => 'spree/admin/shared/sub_menu/_product',
     :name => 'add_property_type_to_admin_menu',
     :insert_bottom => "[data-hook='admin_product_sub_tabs']",
-    :text => "<%= configurations_sidebar_menu_item t('property_types'), admin_property_types_path %>"
+    :text => "<%= configurations_sidebar_menu_item t('property_types'), spree.admin_property_types_path %>"
 )
