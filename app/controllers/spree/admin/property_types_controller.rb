e6b1e2ea51bc92aa12a8b06bf18c455ac3159597
dragsbaek <tdragsbaek@hotmail.com>
2017-10-04 22:54:39 +0200

    fix after_filter deprecation

--- app/controllers/spree/admin/property_types_controller.rb
+++ app/controllers/spree/admin/property_types_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class PropertyTypesController < ResourceController
 
-      after_filter :set_property_type_show, :only => [:create, :update]
+      after_action :set_property_type_show, :only => [:create, :update]
 
       def set_property_type_show
         list = []
